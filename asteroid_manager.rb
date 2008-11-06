78fd95e4f70d781ef2a0837a99d034bc07bcae12
Rob Caporetto <rcaporetto@gmail.com>
2008-11-06 20:46:48 +1100

    Squashed commit of the following:
    commit 3784bd91bf04e6a0e8738a743c3a00b46888a3f2
    Author: Rob Caporetto <rcaporetto@gmail.com>
    Date:   Thu Nov 6 20:45:22 2008 +1100
    
        Modifying collision code to spawn child asteroids (ie. smaller asteroids) when an asteroid is destroyed.
    
    commit b2c590012a7e3fd3c84bddf8a08147836d5ab481
    Author: Rob Caporetto <rcaporetto@gmail.com>
    Date:   Thu Nov 6 18:34:23 2008 +1100
    
        Adding an overloaded version of spawn_asteroid allowing us to specify the location in addition to the size.
    
    commit fcb146f49e2d1984ba125a55f8eeefbf359073f0
    Author: Rob Caporetto <rcaporetto@gmail.com>
    Date:   Thu Nov 6 18:33:05 2008 +1100
    
        Refactoring out the logic used to initialise an asteroid so that we can reuse it when spawning child asteroids.

--- asteroid_manager.rb
+++ asteroid_manager.rb
@@ -48,24 +48,7 @@ class AsteroidManager
     number_of_asteroids = 11 if number_of_asteroids > 11
     
     number_of_asteroids.times do |counter|
-      asteroid = @asteroid_pool.pop
-
-      asteroid.asteroid_size = :large
-      asteroid.bounding_sphere_radius = @bounding_sphere_radius[:large]
-      asteroid.object_image = @asteroid_images[:large]
-      asteroid.angle = rand(360)
-      asteroid.set_forward_velocity(AsteroidForwardVelocity)
-
-      # Do we want to spawn on the side, or on the top?
-      spawn_horizontal = get_random_boolean
-      if spawn_horizontal then
-        asteroid.location_x = (get_random_boolean ? MinPlayfieldHorizontalBound : MaxPlayfieldHorizontalBound)
-        asteroid.location_y = Conversions.transform_screen_to_world(rand(ScreenHeight), ScreenHeight, MinVisibleVerticalBound)
-      else
-        asteroid.location_y = (get_random_boolean ? MinPlayfieldVerticalBound : MaxPlayfieldVerticalBound)
-        asteroid.location_x = Conversions.transform_screen_to_world(rand(ScreenWidth), ScreenWidth, MinVisibleHorizontalBound)
-      end
-
+      asteroid = spawn_asteroid(:large)
       @active_asteroids << asteroid
     end
   end
@@ -85,9 +68,9 @@ class AsteroidManager
   
   def test_for_collision(object_to_test, remove_asteroid_on_collision)
     collision = false
-    
+
     @active_asteroids.each do |asteroid|
-      radius = object_to_test.bounding_sphere_radius + asteroid.bounding_sphere_radius
+      radius = object_to_test.bounding_sphere_radius.to_i + asteroid.bounding_sphere_radius.to_i
       
       distance = Gosu::distance(
           object_to_test.location_x.to_i,
@@ -97,7 +80,15 @@ class AsteroidManager
 
       collision = (distance <= radius)
       if collision
-        remove_asteroid(asteroid) if remove_asteroid_on_collision
+        if remove_asteroid_on_collision
+          size = asteroid.asteroid_size
+          location_x = asteroid.location_x
+          location_y = asteroid.location_y
+          
+          remove_asteroid(asteroid)
+          spawn_child_asteroids(size, location_x, location_y)
+        end  
+        
         break
       end
     end
@@ -131,4 +122,44 @@ class AsteroidManager
     (rand(2) == 1 ? true : false) 
   end
   
+  def spawn_asteroid(size)
+    asteroid = @asteroid_pool.pop
+
+    asteroid.asteroid_size = size
+    asteroid.bounding_sphere_radius = @bounding_sphere_radius[size]
+    asteroid.object_image = @asteroid_images[size]
+    asteroid.angle = rand(360)
+    asteroid.set_forward_velocity(AsteroidForwardVelocity)
+
+    # Do we want to spawn on the side, or on the top?
+    spawn_horizontal = get_random_boolean
+    if spawn_horizontal then
+      asteroid.location_x = (get_random_boolean ? MinPlayfieldHorizontalBound : MaxPlayfieldHorizontalBound)
+      asteroid.location_y = Conversions.transform_screen_to_world(rand(ScreenHeight), ScreenHeight, MinVisibleVerticalBound)
+    else
+      asteroid.location_y = (get_random_boolean ? MinPlayfieldVerticalBound : MaxPlayfieldVerticalBound)
+      asteroid.location_x = Conversions.transform_screen_to_world(rand(ScreenWidth), ScreenWidth, MinVisibleHorizontalBound)
+    end
+    
+    asteroid
+  end
+  
+  def spawn_child_asteroids(size, location_x, location_y)
+    child_size = get_child_size_for_asteroid_size(size)
+    return if child_size == nil
+    2.times do
+      asteroid = spawn_asteroid(child_size)
+      asteroid.location_x = location_x
+      asteroid.location_y = location_y
+      @active_asteroids << asteroid
+    end
+  end
+  
+  # I'm not the biggest fan of this function's implementation, but well
+  # I'm a bit stumped for ideas :)
+  def get_child_size_for_asteroid_size(size)
+    return :medium if size == :large
+    return :small if size == :medium
+    return nil if size == :small
+  end
 end
